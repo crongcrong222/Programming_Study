ea5e1d742150118e9a3fc44d272e5c571c130af0
erica cha <crongcrong222@gmail.com>
2021-01-13 09:13:36 +0900

    commit main

--- 01Algorithm_Lecture/NDB_Algoritm/08_merge_sort.cpp
+++ 01Algorithm_Lecture/NDB_Algoritm/08_merge_sort.cpp
@@ -1,53 +1,65 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int number = 8;
-int sorted[8];
+// int number = 8;
+// int sorted[8];
 
-void merge(int a[], int m, int middle, int n){
-	int i = m;
-	int j = middle + 1;
-	int k = m;
-	while(i <= middle && j <= n){
-		if(a[i]<a[j]){
-			sorted[k] = a[i];
-			i++;
-		} else{
-			sorted[k] = a[j];
-			j++;
-		}
-		k++;
-	}
-	// ³²Àº µ¥ÀÌÅÍ¸¦ »ğÀÔ
-	if(i > middle){
-		for(int t = j; t <= n; t  ++){
-			sorted[k] = a[t];
-			k++;
-		}
-	} else{
-		for(int t = i; t <= middle; t ++){
-			sorted[k] = a[i];
-			k++;
-		}
-	}// Á¤·ÄµÈ ¹è¿­À» »ğÀÔ
-	for(int t = m; t <= n; t ++){
-		a[t] = sorted[t];
-	} 
-}
+// void merge(int a[], int m, int middle, int n){
+// 	int i = m;
+// 	int j = middle + 1;
+// 	int k = m;
+// 	while(i <= middle && j <= n){
+// 		if(a[i]<a[j]){
+// 			sorted[k] = a[i];
+// 			i++;
+// 		} else{
+// 			sorted[k] = a[j];
+// 			j++;
+// 		}
+// 		k++;
+// 	}
+// 	// ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Í¸ï¿½ ï¿½ï¿½ï¿½ï¿½
+// 	if(i > middle){
+// 		for(int t = j; t <= n; t  ++){
+// 			sorted[k] = a[t];
+// 			k++;
+// 		}
+// 	} else{
+// 		for(int t = i; t <= middle; t ++){
+// 			sorted[k] = a[i];
+// 			k++;
+// 		}
+// 	}// ï¿½ï¿½ï¿½Äµï¿½ ï¿½è¿­ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+// 	for(int t = m; t <= n; t ++){
+// 		a[t] = sorted[t];
+// 	} 
+// }
 
-void mergeSort(int a[], int m, int n){
-	if(m < n){
-		int middle = (m + n) / 2;
-		mergeSort(a,m,middle);
-		mergeSort(a,middle+1,n);
-		merge(a,m,middle,n);
-	}
-}
+// void mergeSort(int a[], int m, int n){
+// 	if(m < n){
+// 		int middle = (m + n) / 2;
+// 		mergeSort(a,m,middle);
+// 		mergeSort(a,middle+1,n);
+// 		merge(a,m,middle,n);
+// 	}
+// }
 
-int main(){
-	int array[number] = {7,6,5,8,3,5,9,1};
-	mergeSort(array,0,number-1);
-	for(int i = 0; i < number ; i ++){
-		printf("%d ",array[i]);
-	}
+// int main(){
+// 	int array[number] = {7,6,5,8,3,5,9,1};
+// 	mergeSort(array,0,number-1);
+// 	for(int i = 0; i < number ; i ++){
+// 		printf("%d ",array[i]);
+// 	}
+// 	return 0;
+// }
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int a;
+	cin >> a ;
+	cout << a << " " << a <<endl;
 	return 0;
 }
--- 01Algorithm_Lecture/NDB_Algoritm/10_stl_1.cpp
+++ 01Algorithm_Lecture/NDB_Algoritm/10_stl_1.cpp
@@ -9,10 +9,10 @@ bool compare(const pair<int, string> &a, const pair<int, string> &b){
 }
 int main(){
 	vector<pair<int, string> > v;
-	v.push_back(pair<int, string>(90,"나동빈"));
-	v.push_back(pair<int, string>(50,"차애리"));
-	v.push_back(pair<int, string>(20,"박의준"));
-	v.push_back(pair<int, string>(100,"서리안"));
+	v.push_back(pair<int, string>(90,"111"));
+	v.push_back(pair<int, string>(50,"2222"));
+	v.push_back(pair<int, string>(20,"3333"));
+	v.push_back(pair<int, string>(100,"4444"));
 	
 	sort(v.begin(),v.end(),compare);
 	for(int i = 0; i < v.size(); i ++){
